d7abc4675d465a9f473b3060e8a07cc7bb64c532
Amit barwal <tamit700@gmail.com>
2020-04-03 17:16:14 +0530

    yes
    change in POM and Use Swagger for Documentation and integrating with MVC

---
+++ src/main/java/com/niit/myblog/model/Value.java
@@ -0,0 +1,28 @@
+package com.niit.myblog.model;
+
+
+public class Value {
+	private long id;
+	private String quote;
+	
+	public Value() {
+		
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getQuote() {
+		return quote;
+	}
+	public void setQuote(String quote) {
+		this.quote = quote;
+	}
+	@Override
+	public String toString() {
+		return "Value [id=" + id + ", quote=" + quote + "]";
+	}
+	
+}
